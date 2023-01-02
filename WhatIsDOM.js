08150b4bb2ea7761474a49db2b7349ae0fa51413
pratapshingane18 <sushrutrao465@gmail.com>
2023-01-02 18:03:32 +0530

    adding what is DOM file

---
+++ WhatIsDOM.js
@@ -0,0 +1,30 @@
+/* What is DOM 
+
+Document Object Model
+
+* Tree of Nodes/Elements created by the browser
+* JavaScript can be used to read, write and Manipulate to the DOM
+* It is the Object Oriendted Representation
+
+                            Document
+                               |
+                            Root Element
+                                <html>
+                                |
+                 -------------------------------------               
+                 |                                   |
+               Element                              Element
+                <head>                               <body>
+                  |                                    |
+               Element                        ----------------------
+                <title>       Attributes ----- Element           Element
+                                "href"          <a>                 <h1>
+                   |                             |                    |
+                Text                            Text                Text
+            "my title"                         "My link"            "My header"    
+
+
+
+
+
+*/
