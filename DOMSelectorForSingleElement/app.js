bb030ca20d665ecaa693df945727ea8103174266
pratapshingane18 <sushrutrao465@gmail.com>
2023-01-03 14:29:36 +0530

    adding Examing DOM dinle element

--- DOMSelectorForSingleElement/app.js
+++ DOMSelectorForSingleElement/app.js
@@ -1,3 +1,46 @@
+// JQuery is never Suggested for DOM manipulation::
+// Vanila Javascript is best for DOM manipulation
+
+/* Single ELement Selector- Only Select First out of the COllection */
+
+//document.getElementById(); // Select Element with its ID
+
+console.log(document.getElementById('task-title'));
+
+// Get things from the element:
+console.log(document.getElementById('task-title').id);
+console.log(document.getElementById('task-title').class);
+
+// Change Styling
+document.getElementById('task-title').style.background='#787';
+document.getElementById('task-title').style.color='#923';
+
+document.getElementById('task-title').style.color.padding='5px';
+// document.getElementById('task-title').style.display='none';
+
+// Change the Content
+document.getElementById('task-title').innerText = 'My Task';
+
+// Inner Html is used - when we wanna fetch something through the fetch api or ajax and then you wanted to insert a chunk of html;
+document.getElementById('task-title').innerHTML = '<span style="color:red">Task List<span> ';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // document.getElementById()
 
 // console.log(document.getElementById('task-title'));
