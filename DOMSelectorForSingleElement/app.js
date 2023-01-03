6f0c5ea0d961ad1a672c444f83eb5cbe7f94345f
pratapshingane18 <sushrutrao465@gmail.com>
2023-01-03 14:44:50 +0530

    adding Examing DOM sinle element done

--- DOMSelectorForSingleElement/app.js
+++ DOMSelectorForSingleElement/app.js
@@ -12,6 +12,36 @@ console.log(document.getElementById('task-title').id);
 console.log(document.getElementById('task-title').class);
 
 // Change Styling
+// document.getElementById('task-title').style.background='#787';
+// document.getElementById('task-title').style.color='#923';
+
+// document.getElementById('task-title').style.color.padding='5px';
+// // document.getElementById('task-title').style.display='none';
+
+// // Change the Content
+// document.getElementById('task-title').innerText = 'My Task';
+
+// // Inner Html is used - when we wanna fetch something through the fetch api or ajax and then you wanted to insert a chunk of html;
+// document.getElementById('task-title').innerHTML = '<span style="color:red">Task List<span> ';
+
+// Or to avoid writting document.getElementById('task-title) Repeatedly make variable for it
+
+const TaskTitle = document.getElementById('task-title');
+
+TaskTitle.style.background='#787';
+TaskTitle.style.color='#923';
+
+TaskTitle.style.color.padding='5px';
+// document.getElementById('task-title').style.display='none';
+
+// Change the Content
+TaskTitle.innerText = 'My Task';
+
+// Inner Html is used - when we wanna fetch something through the fetch api or ajax and then you wanted to insert a chunk of html;
+TaskTitle.innerHTML = '<span style="color:red">Task List<span> ';
+
+
+
 document.getElementById('task-title').style.background='#787';
 document.getElementById('task-title').style.color='#923';
 
@@ -25,6 +55,25 @@ document.getElementById('task-title').innerText = 'My Task';
 document.getElementById('task-title').innerHTML = '<span style="color:red">Task List<span> ';
 
 
+
+// Newer and Much More powerful
+
+// document.querySelector()
+
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('card-title'));
+console.log(document.querySelector('h5'));
+
+document.querySelector('li').style.color = 'red'; // It will only style first li out of all the li as it is only  single query selector
+
+document.querySelector('ul li').style.color='blue';
+document.querySelector('li:last-child').style.color='orange';
+document.querySelector('li:nth-child(3').style.color='yellow';
+
+document.querySelector('li:nth-child(4)').textContent = "Hello Tasks";
+
+document.querySelector('li:nth-child(odd').style.color='green'; // It will only change first Odd the reason being it is only a single query selector
+// Same in case of even
 
 
 
